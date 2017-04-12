e19782c994ce8f5d55fe675678c3acc8eb4d1f48
root <root@cea2b93bf2f3>
2017-04-12 17:35:58 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,13 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "PJD-IBM-41217",
+  "homePage" : "Main",
+  "name" : "PJD_IBM_41217",
+  "platformType" : "WEB",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
